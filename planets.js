1d1a75bfeab75e38a10dbef29b40d3780cfa177d
Jacob <jacobmofficial@outlook.com>
2025-11-17 17:32:52 +0000

    moved_some_i_guess

--- planets.js
+++ planets.js
@@ -21,12 +21,10 @@ new p5((p) => {
 		venus = new Planets(0, 0, 40, "orange", 170, 170, 0.8);
 		earth = new Planets(0, 0, 80, "blue", 250, 250, 0.7);
 		mars = new Planets(0, 0, 40, "orange", 350, 350, 0.6);
-		jupiter = new Planets(0, 0, 150, "orange", 380, 0.5);
+		jupiter = new Planets(0, 0, 130, "orange", 380, 380, 0.5);
 		saturn = new Planets(0, 0, 80, "brown", 530, 530, 0.4);
 		uranus = new Planets(0, 0, 80, "blue", 630, 630, 0.3);
 		neptune = new Planets(0, 0, 80, "blue", 730, 730, 0.2);
-		moon = new Moons(0, 0, 80, "green", 730, 730, 0.2);
-	
 
 	}
 
@@ -42,30 +40,15 @@ new p5((p) => {
 		}
 
 		drawPlanets() {
-			// this is hard coded i need it to my dynamic for each planet
-			// I realise the issue i just don't knwo how to fix it, i mean
-			// i could attempt to use translate()
-			//
+
 			this.x = this.rx * p.cos(p.frameCount * 0.2 * this.s) + 950;
 			this.y = this.ry * p.sin(p.frameCount * 0.2 * this.s) + 900;
 
-			// p.translate(this.rx, this.ry)
 			p.fill(this.c);
-
 			p.circle(this.x, this.y, this.d); 
 			
-			console.log(this.x, this.y)
+			// console.log(this.x, this.y)
 
-		}
-	}
-
-	class Moons extends Planets {
-		drawMoon() {
-
-			this.x = this.rx * p.cos(p.frameCount * 0.2 * this.s) + 950;
-			this.y = this.ry * p.sin(p.frameCount * 0.2 * this.s) + 900;
-			p.fill(this.c)
-			p.circle(this.x, this.y, this.d);
 		}
 	}
 
@@ -81,13 +64,11 @@ new p5((p) => {
 		saturn.drawPlanets();
 		uranus.drawPlanets();
 		neptune.drawPlanets();
-
-		moon.drawMoon();
-
 	}
 
 })
 
-// TODO
-// 1. add names over planets
-// 2. making a line that gets drawn followin the planet rotation
+
+//     I need each planet to be "solo" in a senses i can choose which planet or moon to rotate around
+//     	any for example all the planets use the suns position to rotate around and then i can use each planet 
+//     	for the moon rotations in the same way 
