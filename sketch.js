c9a18fa0231debdde93cdd9eec1d76e319b9535d
noahpro <noahpro@gmail.com>
2025-11-17 19:27:35 -0500

    bun

--- sketch.js
+++ sketch.js
@@ -2,71 +2,67 @@ let circle;
 
 new p5((p) => {
   p.setup = () => {
+    p.createCanvas(800, 800);
+    circle = new Circle(100, 100, 100, 100);
 
-	p.createCanvas(800, 800);
-	circle = new Circle(100, 100, 100, 100);
+    // coloring
+    let c = p.random(["red", "blue", "green"]);
+    p.strokeWeight(10);
+    p.stroke("white");
+    p.fill(c);
+  };
+  // p.moving_circle = () => {
+  //
+  //  let x = 90 * p.cos(p.frameCount * 2) + 50;
+  //  let y = 50 * p.sin(p.frameCount * 2) + 50;
+  //  circle = new Ellipse(100, 100, 100, 100)
+  //
+  // };
 
-	// coloring
-	let c = p.random(['red', 'blue', 'green']);
-	p.strokeWeight(10);
-	p.stroke("white");
-	p.fill(c);
+  class Circle {
+    constructor(x, y, h, w) {
+      this.x = x;
+      this.y = y;
+      this.h = h;
+      this.w = w;
+    }
 
-};
- // p.moving_circle = () => {
- //
- //  let x = 90 * p.cos(p.frameCount * 2) + 50;
- //  let y = 50 * p.sin(p.frameCount * 2) + 50;
- //  circle = new Ellipse(100, 100, 100, 100)
- //
- // };
+    circle1() {
+      let x = 100 * p.cos(p.frameCount * 0.01 * 2) + 400;
+      let y = 100 * p.cos(p.frameCount * 0.03 * 2) + 400;
+      p.circle(x, y, this.h, this.w);
+    }
 
- class Circle {
-	constructor(x, y, h, w) {
-		this.x = x;
-		this.y = y;
-		this.h = h;
-		this.w = w;
-	}
+    circle2() {
+      let x = 100 * p.cos(p.frameCount * 0.03) + 400;
+      let y = 100 * p.cos(p.frameCount * 0.01) + 400;
+      p.circle(x, y, this.h, this.y);
+    }
+  }
 
-	 circle1() {
-		let x = 100 * p.cos(p.frameCount * 0.01 * 2) + 400; 
-		let y = 100 * p.cos(p.frameCount * 0.03 * 2) + 400; 
-		p.circle(x, y, this.h, this.w);
-	 }
-
-	 circle2() {
-		let x = 100 * p.cos(p.frameCount * 0.03) + 400; 
-		let y = 100 * p.cos(p.frameCount * 0.01) + 400; 
-		p.circle(x, y, this.h, this.y);
-	 }
-}
-
- p.draw = () => {
+  p.draw = () => {
     p.background(0);
-	circle.circle1();
-	circle.circle2();
-	
-	console.log(circle.circle2);
-	// p.moving_circle();
+    circle.circle1();
+    circle.circle2();
+
+    console.log(circle.circle2);
+    // p.moving_circle();
   };
 });
 
-
-
-	//  p.test_circle = () => {
-	//
-	// let c = p.color(0, 0, 255);
-	// p.strokeWeight(10);
-	// p.stroke("red");
-	//
-	// let x = 90 * p.cos(p.frameCount * 2) + 50;
-	// let y = 50 * p.sin(p.frameCount * 2) + 50;
-	// p.angleMode(p.DEGREES);
-	//
-	// p.fill(c);
-	// p.circle(x, y, 50, 50);
-	//
-	//    // console.log(x, y);
-	//
-	//  }
+//  p.test_circle = () => {
+//
+// let c = p.color(0, 0, 255);
+// p.strokeWeight(10);
+// p.stroke("red");
+//
+// let x = 90 * p.cos(p.frameCount * 2) + 50;
+// let y = 50 * p.sin(p.frameCount * 2) + 50;
+// p.angleMode(p.DEGREES);
+//
+// p.fill(c);
+// p.circle(x, y, 50, 50);
+//
+//    // console.log(x, y);
+//
+//  }
