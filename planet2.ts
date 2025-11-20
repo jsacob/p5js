57d132938133bed94c194d865e339d170a64d900
Jacob <jacobmofficial@outlook.com>
2025-11-20 01:20:24 +0000

    okay another thing

--- planet2.ts
+++ planet2.ts
@@ -20,16 +20,16 @@ new p5((p) => {
     diameter: number;
     speed: number;
     mass: number;
-    static gravConstant = 6.674;
+    static gravConstant = 6.674 * Math.pow(10, -11);
     color: string;
 
-    constructor(x = 0, y = 0, radius = 0, _mass = 0, speed = 0, color = "") {
+    constructor(x = 0, y = 0, radius = 0, mass = 0, speed = 0, color = "") {
       this.x = x;
       this.y = y;
       this.radius = radius;
       this.diameter = radius * 2;
       this.speed = speed;
-      this.mass = 0;
+      this.mass = mass;
       this.color = color;
     }
 
@@ -40,9 +40,30 @@ new p5((p) => {
   }
 
   p.draw = () => {
-    sun.x = p.cos(p.frameCount * sun.speed) + 300;
-    sun.y = p.sin(p.frameCount * sun.speed) + 300;
+    // sun.x = p.cos(p.frameCount * sun.speed) + 300;
+    // sun.y = p.sin(p.frameCount * sun.speed) + 300;
+
+    // take the suns x and mercury x
+    // then suns y and mercury y
+    // square them first, then subtract them
+    // then add the two outcomes
+    // then square
+    //
+
+    let sx = sun.x - mercury.x;
+    let sy = sun.y - mercury.y;
+    let px = Math.pow(sx, 2);
+    let py = Math.pow(sy, 2);
+    let total = px + py;
+    let distance = Math.sqrt(total);
+    console.log(distance);
+
+    let F =
+      ((Planet.gravConstant * (sun.mass * mercury.mass)) / distance) * distance;
+    // console.log(sun.mass, mercury.mass);
+    console.log(F);
 
     sun.draw();
     mercury.draw();
+  };
 });
