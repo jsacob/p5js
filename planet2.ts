e9731cf4a99b6b4189801affad010085b7283446
Jacob <jacobmofficial@outlook.com>
2025-11-22 19:17:55 +0000

    update

--- planet2.ts
+++ planet2.ts
@@ -4,6 +4,7 @@ let sun: any;
 let mercury: any;
 let venus: any;
 let earth: any;
+let mars: any;
 
 let sunMass = 4.545 * Math.pow(10, 13);
 
@@ -16,9 +17,10 @@ new p5((p) => {
     p.background(0);
 
     sun = new Planet(0, 0, 50, sunMass, 0, 0, "yellow");
-    mercury = new Planet(150, 150, 20, 1, 0, 0, "grey");
+    mercury = new Planet(150, 150, 20, 1, 1.5, -2.5, "grey");
     venus = new Planet(200, 200, 30, 1, 2, -2, "orange");
     earth = new Planet(250, 250, 40, 1, 1, -1, "green");
+    mars = new Planet(350, 350, 20, 1, 1, -1, "orange");
   };
 
   class Planet {
@@ -58,44 +60,43 @@ new p5((p) => {
   }
 
   function physicsRework(planet: Planet, sun: Planet) {
-    let dx = sun.x - planet.x;
-    let dy = sun.y - planet.y;
-    let distance = Math.sqrt(dx * dx + dy * dy);
+    let diffx = sun.x - planet.x;
+    let diffy = sun.y - planet.y;
+    let distance = Math.sqrt(diffx * diffx + diffy * diffy);
 
     let Force =
       (Planet.gravConstant * sun.mass * planet.mass) / (distance * distance);
 
-    let Force_x = Force * (dx / distance);
-    let Force_y = Force * (dy / distance);
+    let unit_x = diffx / distance;
+    let unit_y = diffy / distance;
 
-    let Acceleration_x = Force_x / planet.mass;
-    let Acceleration_y = Force_y / planet.mass;
+    let acceleration = Force / planet.mass;
 
-    // let Velocity_x = Math.sqrt((Planet.gravConstant * sun.mass) / distance);
-    // let Velocity_y = Math.sqrt((Planet.gravConstant * sun.mass) / distance);
-    // let Acceleration_x = Math.pow(Velocity_x, 2) / distance;
-    // let Acceleration_y = Math.pow(Velocity_y, 2) / distance;
+    let acceleration_x = acceleration * unit_x;
+    let acceleration_y = acceleration * unit_y;
 
-    let dt = 1;
-    planet.velocity_x += Acceleration_x * dt;
-    planet.velocity_y += Acceleration_y * dt;
+    let dt = 0.5;
+    planet.velocity_x += acceleration_x * dt;
+    planet.velocity_y += acceleration_y * dt;
 
-    planet.x += planet.velocity_x;
-    planet.y += planet.velocity_y;
+    planet.x += planet.velocity_x * dt;
+    planet.y += planet.velocity_y * dt;
   }
 
   p.draw = () => {
     p.clear();
-    p.background(40);
+    p.background(255);
 
     physicsRework(mercury, sun);
     physicsRework(venus, sun);
     physicsRework(earth, sun);
+    physicsRework(mars, sun);
 
     console.log(mercury.x, mercury.y);
     sun.draw();
     mercury.draw();
     venus.draw();
     earth.draw();
+    mars.draw();
   };
 });
